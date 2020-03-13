3f31f8b26cc8f923c37171a26deab2cf0b4bcdfb
Nelmeris <kufaevartem@icloud.com>
2020-03-13 23:13:02 +0300

    [#17-ticket-list] Модель биржи

---
+++ LocalPods/TicketList/TicketList/Classes/Exchange.swift
@@ -0,0 +1,12 @@
+//
+//  Exchange.swift
+//  Pods-TicketList_Example
+//
+//  Created by Artem Kufaev on 13.03.2020.
+//
+
+public struct Exchange: Decodable {
+    let name: String
+    let code: String
+    let currency: String
+}
