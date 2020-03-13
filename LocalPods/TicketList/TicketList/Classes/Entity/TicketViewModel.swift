50a5e4d99d64a499b0a5f8c4acb43dcbfddb744a
Nelmeris <kufaevartem@icloud.com>
2020-03-13 23:49:51 +0300

    [#17-ticket-list] ViewModel для будущей таблицы

--- LocalPods/TicketList/TicketList/Classes/Entity/TicketViewModel.swift
+++ LocalPods/TicketList/TicketList/Classes/Entity/TicketViewModel.swift
@@ -5,4 +5,19 @@
 //  Created by Artem Kufaev on 13.03.2020.
 //
 
-import Foundation
+struct TicketViewModel {
+    let symbol: String
+    let description: String
+}
+
+class TicketViewModelFabric {
+    static func make(from models: [Ticket]) -> [TicketViewModel] {
+        return models.map(make)
+    }
+    
+    static func make(from model: Ticket) -> TicketViewModel {
+        let symbol = model.symbol
+        let description = model.description
+        return TicketViewModel(symbol: symbol, description: description)
+    }
+}
