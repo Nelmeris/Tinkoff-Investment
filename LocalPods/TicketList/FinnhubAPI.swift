aa693e7e5c03bafdb9197e6034b69bd70ba52861
Nelmeris <kufaevartem@icloud.com>
2020-03-13 23:09:43 +0300

    [#17-ticket-list] Изменено название API

--- LocalPods/TicketList/FinnhubAPI.swift
+++ LocalPods/TicketList/FinnhubAPI.swift
@@ -1,12 +1,13 @@
 //
-//  TicketAPI.swift
+//  FinnhubAPI.swift
 //  Pods-TicketList_Example
 //
 //  Created by Artem Kufaev on 13.03.2020.
 //
 
-public enum TicketAPI {
+public enum FinnhubAPI {
     case stockSymbol(exchange: String)
+    case stockExchange
     
     var apiKey: String { return "bpkpa07rh5rcgrlrc06g" }
 }
