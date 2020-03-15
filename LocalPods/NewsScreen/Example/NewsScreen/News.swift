c374c7375a559a6a1dbe3a82fe684b19e8c236f9
Nelmeris <kufaevartem@icloud.com>
2020-03-15 06:59:10 +0300

    [#27-news] Модель из интернета

---
+++ LocalPods/NewsScreen/Example/NewsScreen/News.swift
@@ -0,0 +1,18 @@
+//
+//  News.swift
+//  NewsScreen_Example
+//
+//  Created by Artem Kufaev on 15.03.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+struct News: Decodable {
+    let id: Int
+    let image: String
+    let headline: String
+    let summary: String
+    let datetime: TimeInterval
+    let url: URL
+}
