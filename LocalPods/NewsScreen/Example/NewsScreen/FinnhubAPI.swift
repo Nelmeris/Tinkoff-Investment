1014578ad88df27e8007d013f591af33e54b3085
Nelmeris <kufaevartem@icloud.com>
2020-03-15 06:59:38 +0300

    [#27-news] Апи для загрузки новостей

---
+++ LocalPods/NewsScreen/Example/NewsScreen/FinnhubAPI.swift
@@ -0,0 +1,34 @@
+//
+//  FinnhubAPI.swift
+//  NewsScreen_Example
+//
+//  Created by Artem Kufaev on 15.03.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Network
+
+enum FinnhubAPI: INetworkAPI {
+    case news
+    
+    var apiKey: String { "bpkpa07rh5rcgrlrc06g" }
+    var schema: NetworkSchema { .https }
+    var host: String { "finnhub.io/api/v1" }
+    var httpMethod: HTTPMethod { .get }
+    var headers: HTTPHeaders? { nil }
+    
+    var path: String {
+        switch self {
+        case .news: return "news"
+        }
+    }
+    
+    var task: HTTPTask { .requestParameters(
+        bodyParameters: nil,
+        urlParameters: [
+            "category": "general",
+            "token": apiKey
+        ])
+    }
+    
+}
