be1fff4187498ca4c6c8bb21e12978ecd1d54534
Nelmeris <kufaevartem@icloud.com>
2020-03-15 06:58:56 +0300

    [#27-news] Модель для отображения

---
+++ LocalPods/NewsScreen/Example/NewsScreen/NewsViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  NewsViewModel.swift
+//  NewsScreen_Example
+//
+//  Created by Artem Kufaev on 15.03.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+struct NewsViewModel: Identifiable {
+    let id: Int
+    let imageURL: String
+    let headline: String
+    let summary: String
+    let date: String
+    let url: String
+}
+
+class NewsViewModelFactory {
+    
+    func make(from models: [News]) -> [NewsViewModel] {
+        return models.map(make)
+    }
+    
+    func make(from model: News) -> NewsViewModel {
+        let id = model.id
+        let imageURL = model.image
+        let headline = model.headline
+        let summary = model.summary
+        let dateFormatter = DateFormatter.unix()
+        let date = Date(timeIntervalSince1970: model.datetime)
+        let dateStr = dateFormatter.string(from: date)
+        let url = model.url.absoluteString
+        return NewsViewModel(id: id,
+                             imageURL: imageURL,
+                             headline: headline,
+                             summary: summary,
+                             date: dateStr,
+                             url: url)
+    }
+    
+}
