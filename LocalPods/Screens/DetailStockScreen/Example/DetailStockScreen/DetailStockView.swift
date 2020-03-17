b278afc63bd2aa839dde0bd712514bad39a0197d
Nelmeris <kufaevartem@icloud.com>
2020-03-17 23:15:52 +0300

    [#29-detail-stock] Создана связующая View

---
+++ LocalPods/Screens/DetailStockScreen/Example/DetailStockScreen/DetailStockView.swift
@@ -0,0 +1,30 @@
+//
+//  DetailStockView.swift
+//  DetailStockScreen_Example
+//
+//  Created by Artem Kufaev on 17.03.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailStockView: View {
+    let companySymbol: String
+    
+    init(with companySymbol: String) {
+        self.companySymbol = companySymbol
+    }
+    
+    var body: some View {
+        VStack {
+            CompanyProfileView(with: self.companySymbol)
+            CompanyNewsView(with: self.companySymbol)
+        }.navigationBarTitle(self.companySymbol)
+    }
+}
+
+struct DetailStockView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailStockView(with: "AAPL")
+    }
+}
