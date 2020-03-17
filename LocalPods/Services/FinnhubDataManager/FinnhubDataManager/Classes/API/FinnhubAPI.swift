d2dcb12ab3c9ca0db978e75e378c642e1a35ea77
Nelmeris <kufaevartem@icloud.com>
2020-03-17 20:03:10 +0300

    [#29-detail-stock] Новые методы API
    Новости и профайл компании по символу

--- LocalPods/Services/FinnhubDataManager/FinnhubDataManager/Classes/API/FinnhubAPI.swift
+++ LocalPods/Services/FinnhubDataManager/FinnhubDataManager/Classes/API/FinnhubAPI.swift
@@ -11,6 +11,8 @@ public enum FinnhubAPI {
     case stockSymbol(exchange: String)
     case stockExchange
     case news
+    case companyNews(symbol: String)
+    case companyProfile(symbol: String)
 
     var apiKey: String { "bpkpa07rh5rcgrlrc06g" }
 }
@@ -26,12 +28,19 @@ extension FinnhubAPI: INetworkAPI {
         case .stockSymbol: return "/stock/symbol"
         case .stockExchange: return "/stock/exchange"
         case .news: return "news"
+        case .companyProfile: return "/stock/profile"
+        case .companyNews(let symbol): return "news/\(symbol)"
         }
     }
 
     public var httpMethod: HTTPMethod {
         switch self {
-        case .stockSymbol, .stockExchange, .news: return .get
+        case .stockSymbol,
+             .stockExchange,
+             .news,
+             .companyProfile,
+             .companyNews:
+            return .get
         }
     }
 
@@ -48,6 +57,13 @@ extension FinnhubAPI: INetworkAPI {
             return .requestParameters(bodyParameters: nil,
                                       urlParameters: ["category": "general",
                                                       "token": apiKey])
+        case .companyProfile(let symbol):
+            return .requestParameters(bodyParameters: nil,
+                                      urlParameters: ["symbol": symbol,
+                                                      "token": apiKey])
+        case .companyNews:
+            return .requestParameters(bodyParameters: nil,
+                                      urlParameters: ["token": apiKey])
         }
     }
 
