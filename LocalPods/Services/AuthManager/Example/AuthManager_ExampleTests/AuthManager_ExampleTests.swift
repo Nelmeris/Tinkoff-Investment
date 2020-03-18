49605ecac1905f6e293d8da59b34c7d3d3700428
Nelmeris <kufaevartem@icloud.com>
2020-03-18 15:52:27 +0300

    [#30-tests] Создан KeychainMock

--- LocalPods/Services/AuthManager/Example/AuthManager_ExampleTests/AuthManager_ExampleTests.swift
+++ LocalPods/Services/AuthManager/Example/AuthManager_ExampleTests/AuthManager_ExampleTests.swift
@@ -16,7 +16,8 @@ class AuthManagerTests: XCTestCase {
     var keychain: IKeychain!
     
     override func setUp() {
-        
+        self.keychain = KeychainMock()
+        self.auth = AuthManager(keychain: keychain)
     }
 
     func testExample() {
@@ -24,4 +25,43 @@ class AuthManagerTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+}
+
+class KeychainMock: IKeychain {
+    var invokedSave = false
+    var invokedSaveCount = 0
+    var invokedSaveParameters: (key: String, string: String)?
+    var invokedSaveParametersList = [(key: String, string: String)]()
+    var stubbedSaveResult: OSStatus!
+    func save(key: String, string: String) -> OSStatus {
+        invokedSave = true
+        invokedSaveCount += 1
+        invokedSaveParameters = (key, string)
+        invokedSaveParametersList.append((key, string))
+        return stubbedSaveResult
+    }
+    var invokedRemove = false
+    var invokedRemoveCount = 0
+    var invokedRemoveParameters: (key: String, Void)?
+    var invokedRemoveParametersList = [(key: String, Void)]()
+    var stubbedRemoveResult: OSStatus!
+    func remove(key: String) -> OSStatus {
+        invokedRemove = true
+        invokedRemoveCount += 1
+        invokedRemoveParameters = (key, ())
+        invokedRemoveParametersList.append((key, ()))
+        return stubbedRemoveResult
+    }
+    var invokedLoad = false
+    var invokedLoadCount = 0
+    var invokedLoadParameters: (key: String, Void)?
+    var invokedLoadParametersList = [(key: String, Void)]()
+    var stubbedLoadResult: String!
+    func load(key: String) -> String? {
+        invokedLoad = true
+        invokedLoadCount += 1
+        invokedLoadParameters = (key, ())
+        invokedLoadParametersList.append((key, ()))
+        return stubbedLoadResult
+    }
 }
