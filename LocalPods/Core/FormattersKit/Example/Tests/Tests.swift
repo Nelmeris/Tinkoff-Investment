d1f167e6dc7c2a5b24c25dde1be7bf2de198efb7
Nelmeris <kufaevartem@icloud.com>
2020-03-18 14:31:59 +0300

    [#30-tests] Unit тестирование Formatters

--- LocalPods/Core/FormattersKit/Example/Tests/Tests.swift
+++ LocalPods/Core/FormattersKit/Example/Tests/Tests.swift
@@ -1,50 +1,16 @@
-// https://github.com/Quick/Quick
-
-import Quick
-import Nimble
+import XCTest
 import FormattersKit
 
-class TableOfContentsSpec: QuickSpec {
-    override func spec() {
-        describe("these will fail") {
-
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
-            
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    DispatchQueue.main.async {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        Thread.sleep(forTimeInterval: 0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
-            }
-        }
+class ForattersTests: XCTestCase {
+    
+    override class func setUp() {
+        
     }
+    
+    func testUnix() {
+        let formatter = DateFormatter.unix(with: .newsFeedCell)
+        let string = formatter.string(fromUnixTimestamp: 1569550360)
+        XCTAssertEqual(string, "02:12 19/09/27")
+    }
+    
 }
