1cf3444213ab362ff8da199178181574962a8d4b
Nelmeris <kufaevartem@icloud.com>
2020-03-18 16:35:35 +0300

    [#30-tests] Создан таргет для тестов FinnhubDM

---
+++ LocalPods/Services/FinnhubDataManager/Example/FinnhubDataManager_ExampleTests/FinnhubDataManagerTests.swift
@@ -0,0 +1,21 @@
+//
+//  FinnhubDataManager_ExampleTests.swift
+//  FinnhubDataManager_ExampleTests
+//
+//  Created by Artem Kufaev on 18.03.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class FinnhubDataManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
